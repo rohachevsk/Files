1c1867fd11a30234e6b981fe0907d5090e0558ff
Fujitsu <Fujitsu@DESKTOP-UB06KEO>
2025-05-29 21:18:54 +0300

    доделал

--- Files/Files.cpp
+++ Files/Files.cpp
@@ -1,8 +1,47 @@
-#include <iostream>
-using namespace std;
+#include <iostream>  
+#include <cstdio> // Для использования fopen_s  
+using namespace std;  
 
-int main()
-{
-    std::cout << "Hello World!\n";
+struct Student {  
+   char name[50];  
+   int age;  
+   float grade;  
+};  
+
+int main() { 
+    setlocale(LC_ALL, "");
+   cout << "Введіть кількість студентів: ";  
+   int n;  
+   cin >> n;  
+   cin.ignore();  
+
+   Student* students = new Student[n];  
+
+   for (int i = 0; i < n; ++i) {  
+       cout << "Студент " << i + 1 << ":\n";  
+       cout << "Ім'я: ";  
+       cin.getline(students[i].name, 50);  
+       cout << "Вік: ";  
+       cin >> students[i].age;  
+       cout << "Середній бал: ";  
+       cin >> students[i].grade;  
+       cin.ignore();  
+   }  
+
+   FILE* file = nullptr;  
+   errno_t err = fopen_s(&file, "students.dat", "wb"); // Исправлено использование fopen_s  
+   if (err != 0 || !file) {  
+       cout << "Помилка відкриття файлу!\n";  
+       delete[] students;  
+       return 1;  
+   }  
+
+   fwrite(&n, sizeof(int), 1, file);  
+   fwrite(students, sizeof(Student), n, file);  
+   fclose(file);  
+
+   cout << "Дані записано у файл students.dat\n";  
+
+   delete[] students;  
+   return 0;  
 }
-// я доделаю
